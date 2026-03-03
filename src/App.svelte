821f200260fd8b9f9368ee8862d8bd16c9d5e78a
Robin Škába <robin.skaba@gmail.com>
2026-03-03 16:27:31 +0100

    default search working

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,48 @@
-<script>
+<script lang="ts">
   import Header from "./lib/Header.svelte";
   import { onMount } from "svelte";
-  import { fetchWithAuth } from "./lib/util/api";
+  import { fetchFoodSearchResults, type FoodItem } from "./lib/api/food_search";
 
-  let data = [];
+  let foodItems = $state<FoodItem[]>([]);
+  let query = $state("");
 
-  onMount(async () => {
-    try {
-      const response = await fetchWithAuth("/api/foods?size=30&page=0");
-      data = await response.json();
-      console.log("Authenticated response:", data);
-    } catch (e) {
-      console.error("Authenticated fetch failed:", e.message);
-    }
+  $effect(() => {
+    (async () => {
+      try {
+        foodItems = await fetchFoodSearchResults(query);
+        console.log($state.snapshot(foodItems));
+      } catch (e) {
+        console.error(
+          "Authenticated fetch failed:",
+          e instanceof Error ? e.message : String(e),
+        );
+      }
+    })();
   });
 </script>
 
 <Header />
 <main>
   <section class="side-view">
-    <input type="text" class="field" name="search" />
+    <input type="text" class="field" bind:value={query} />
     <ul>
       <hr />
-      <li>
-        <div class="col">
-          <h5 class="item-name">
-            Food name ktere je fakt dlouhe proboha kdo to vymyslel
-          </h5>
-          <span>Brand</span>
-        </div>
-        <span>XYZ kcal</span>
-      </li>
-      <hr />
-      <li>
-        <div class="col">
-          <h5 class="item-name">
-            Food name ktere je fakt dlouhe proboha kdo to vymyslel
-          </h5>
-          <span>Brand</span>
-        </div>
-        <span>XYZ kcal</span>
-      </li>
-      <hr />
+      {#if foodItems.length > 0}
+        {#each foodItems as item}
+          <li>
+            <div class="col">
+              <h5 class="item-name">
+                {item.name}
+              </h5>
+              <span>{item.brand}</span>
+            </div>
+            <span>{item.macronutrients.kcal} kcal</span>
+          </li>
+          <hr />
+        {/each}
+      {:else}
+        <span>No items found</span>
+      {/if}
     </ul>
   </section>
   <section class="main-view">
---
+++ src/lib/api/food_search.ts
@@ -0,0 +1,84 @@
+import { fetchWithAuth } from "../util/api"
+
+interface Macronutrients {
+    kcal: number
+    fats: number
+    carbs: number
+    protein: number
+}
+
+interface Micronutrients {
+    fiber: number
+    sodium: number
+}
+
+interface Serving {
+    id: string
+    name: string
+    amount: number
+}
+
+export interface FoodItem {
+    id: string
+    name: string
+    author: string
+    barcode: string
+    brand: string
+    isMass: boolean
+    imageFilename: string
+    macronutrients: Macronutrients
+    micronutrients: Micronutrients
+    servings: Serving[]
+}
+
+export async function fetchFoodSearchResults(query: string, page = 0): Promise<FoodItem[]> {
+    console.log(`fetching <${query}>`)
+    if (!query.trim()) return [];
+
+    const params = new URLSearchParams({
+        name: query,
+        page: page.toString(),
+        size: "20" // TODO put in config
+    })
+    const endpoint = `/api/foods?${params.toString()}`
+    console.log(endpoint)
+    const response = await fetchWithAuth(endpoint)
+    const raw = await response.json()
+
+    console.log(raw)
+
+    const foodItems = raw.content.map((rawItem: any) => {
+        const macros = rawItem.macronutrients
+        const micros = rawItem.micronutrients
+        const servings = rawItem.servings
+        return {
+            id: rawItem.id,
+            name: rawItem.name,
+            author: rawItem.author,
+            barcode: rawItem.barcode,
+            brand: rawItem.brand,
+            imageFilename: rawItem.imageFilename,
+            isMass: Boolean(rawItem.isMass),
+            macronutrients: {
+                kcal: Number(macros.kcal),
+                protein: Number(macros.protein),
+                fats: Number(macros.fats),
+                carbs: Number(macros.carbs)
+            },
+            micronutrients: {
+                fiber: Number(micros.fiber),
+                sodium: Number(micros.sodium)
+            },
+            servings: servings.map((serving: any) => {
+                return {
+                    id: serving.id,
+                    name: serving.name,
+                    amount: Number(serving.amount)
+                }
+            })
+        }
+    })
+
+    // TODO paging?
+    return foodItems;
+}
